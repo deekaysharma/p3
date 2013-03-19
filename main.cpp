c69c6937dce02f625cd389776ea14917f29e3c37
Dee Kay <dee@dee-Inspiron-N5110.(none)>
2013-03-20 02:22:40 +0530

    first commit

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+
+class  base
+{
+    int x,y;
+   public:
+    void get()
+    {
+       cout<<"Enter two values:";
+       cin>>x>>y;
+    }
+    friend float mean(base ob);
+};
+float mean(base ob)
+{
+   return float(ob.x+ob.y)/2;
+}
+
+int main()
+{
+base ob1;
+    ob1.get();
+    cout<<"\n Mean value is : "<<mean(ob1);
+}
